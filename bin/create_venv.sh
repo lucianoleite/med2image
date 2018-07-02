56564f79c24df419c1f4bbf6c07e2603adf465e1
Luciano <lucianoleite0@gmail.com>
2018-07-02 15:36:21 -0300

    pore label

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -6,7 +6,7 @@ echo "#sudo apt-get install python3-venv"
 python3 -m venv ~/med2image_venv
 
 #ativar virtual env
-source ~/med2image_venv/bin/activate
+. ~/med2image_venv/bin/activate
 
 #instalar as dependências de maneira rápida
 echo "#pip install --upgrade pip"
