ff3380af60318119f7a4a7d8d23625c8b9c68b22
Raphael F <raphaelfv@poli.ufrj.br>
2024-12-02 18:31:03 -0300

    [DEV-4168] Med2image integrado ao py3
    altera pastas onde é criado o venv (coerente com container) e especifica python 3.7 para não usar o 3.10 do container principal para o qual não há suporte

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -2,13 +2,14 @@
 #requirements
 echo "#sudo apt-get install python3-pip"
 echo "#sudo apt-get install python3-venv"
-
-python3 -m venv ~/med2image_venv
+VENV_BASE_FOLDER=/usr/src/app/med2image
+rm -rf $VENV_BASE_FOLDER/med2image_venv
+python3.7 -m venv $VENV_BASE_FOLDER/med2image_venv
 
 #ativar virtual env
-. ~/med2image_venv/bin/activate
+. $VENV_BASE_FOLDER/med2image_venv/bin/activate
 
-#instalar as dependências de maneira rápida
-echo "#pip install --upgrade pip"
+#upgrade do pip dentro do venv para poder rodar com py3.7 mesmo sendo py3.10 no container
+pip3 install --upgrade pip
 pip3 install med2image
 
